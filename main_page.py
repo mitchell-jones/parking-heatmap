139701178793e53d97d7989a049b3d23a7ef6f49
mitchell-jones <46410287+mitchell-jones@users.noreply.github.com>
2022-12-05 19:20:30 -0500

    updated main for deployment

--- main_page.py
+++ main_page.py
@@ -6,6 +6,7 @@ import streamlit as st
 import pymongo
 import certifi
 from datetime import timedelta
+import numpy as np
 
 def get_client():
     # Get user, pass
@@ -70,7 +71,6 @@ def main_page():
     metric_dict = filtered.groupby('name').count().to_dict()['percentAvailable']
     average_avail = filtered.groupby('name')['percentAvailable'].count().mean()
 
-    st.header("")
     st.markdown("""### Number of Writes per Deck
     Small number is difference from the average, {}.""".format(average_avail))
     col1, col2 = st.columns(2)
@@ -98,10 +98,58 @@ def main_page():
 def page2():
     st.markdown("# Historical Analysis")
     st.sidebar.markdown("# Historical Analysis")
+    with st.sidebar:
+        st.selectbox()
 
 def page3():
     st.markdown("# Latest Availability")
     st.sidebar.markdown("# Latest Availability")
+
+    newest_write = data.datetime.max()
+    st.markdown("This page shows the parking deck availability by deck, as of {}.".format(newest_write))
+
+    most_current = data[data.datetime == newest_write].sort_values('percentAvailable', ascending = False)
+
+    def colors_from_values(values, palette_name):
+        # normalize the values to range [0, 1]
+        normalized = (values - min(values)) / (max(values) - min(values))
+        # convert to indices
+        indices = np.round(normalized * (len(values) - 1)).astype(np.int32)
+        # use the indices to get the colors
+        palette = sns.dark_palette('seagreen', len(values))
+        return np.array(palette).take(indices, axis=0)
+
+    fig = plt.figure()
+    ax = sns.barplot(data = most_current, y = 'name', x = 'percentAvailable', 
+        palette = colors_from_values(most_current['percentAvailable'], None)).set(ylabel = 'Deck', title = 'Percent Available by Deck', xlabel = 'Percent Available')
+    st.pyplot(fig)
+
+    most_current.index = most_current.name
+    metric_dict = most_current.to_dict()['percentAvailable']
+    average_avail = most_current.percentAvailable.mean()
+
+    st.markdown("""### Availability per Deck
+    Small number is difference from the average, {}.""".format(average_avail)
+    col1, col2 = st.columns(2)
+    i = 0
+
+    def render_metric(name):
+            dif = metric_dict[name] - average_avail
+            if dif != 0:
+                st.metric(label = name, value = metric_dict[name], delta = dif)
+            else:
+                st.metric(label = name, value = metric_dict[name], delta = dif, delta_color = 'off')
+
+    for name in most_current.name.unique():
+        if i % 2 == 0:
+            with col1:
+                render_metric(name)
+        else:
+            with col2:
+                render_metric(name)
+        i += 1
+
+
 
 page_names_to_funcs = {
     "Uptime Statistics & About": main_page,
